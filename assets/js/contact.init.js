83da36758e1dee7fbc8f5e0c2caa3d16f12748d4
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2024-05-23 18:29:01 +0100

    Update contact.init.js

--- assets/js/contact.init.js
+++ assets/js/contact.init.js
@@ -1,32 +1,40 @@
 // ----- CONTACT ----- //
  
-$('#contact-form').submit(function() {
+$('#contact-form').submit(function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
     var action = $(this).attr('action');
+
     $("#message").slideUp(750, function() {
         $('#message').hide();
 
         $('#submit')
-            .before('')
             .attr('disabled', 'disabled');
 
         $.post(action, {
                 name: $('#name').val(),
                 email: $('#email').val(),
+                subject: $('#subject').val(),
                 comments: $('#comments').val(),
             },
             function(data) {
-                document.getElementById('message').innerHTML = data;
+                // Show the custom success message
+                $('#message').html("<p>Your message has been sent.</p>");
+
                 $('#message').slideDown('slow');
                 $('#cform img.contact-loader').fadeOut('slow', function() {
-                    $(this).remove()
+                    $(this).remove();
                 });
                 $('#submit').removeAttr('disabled');
-                if (data.match('success') != null) $('#cform').slideUp('slow');
+
+                // Optionally, you can handle form success response with the 'data' variable
+                if (data.match('success') != null) {
+                    $('#contact-form').slideUp('slow');
+                }
             }
         );
 
     });
 
-    return false;
-
+    return false; // Prevent the default form submission behavior
 });
