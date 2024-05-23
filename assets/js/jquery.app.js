c1cf0133875d2422cbb67c73c2d07ccb70fefc69
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2024-05-23 17:53:17 +0100

    First comment

---
+++ assets/js/jquery.app.js
@@ -0,0 +1,26 @@
+(function($) {
+    "use strict";
+    
+    // Sticky Navbar
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".sticky").addClass("nav-sticky");
+        } else {
+            $(".sticky").removeClass("nav-sticky");
+        }
+        
+    });
+
+    // Smooth Link
+    $('.nav-item a, .mouse-down a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+})(jQuery);
+
+
