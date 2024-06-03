6d56a253eed186dc98bb664fa922a4a13a1d0e9d
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2024-06-03 16:53:15 +0100

    Update contact.init.js

--- assets/js/contact.init.js
+++ assets/js/contact.init.js
@@ -2,40 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("contact-form");
     var status = document.getElementById("message");
 
-    // Function to validate the form
-    const validateForm = () => {
-        // Get form inputs
-        const nameInput = document.getElementById("name").value.trim();
-        const emailInput = document.getElementById("email").value.trim();
-        const subjectInput = document.getElementById("subject").value.trim();
-        const commentsInput = document.getElementById("comments").value.trim();
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        // Check if any of the required fields are empty
-        if (!nameInput || !emailInput || !subjectInput || !commentsInput) {
-            status.textContent = "Please fill in all the required fields.";
-            return false;
-        }
-
-        // Check for valid email format
-        if (!emailPattern.test(emailInput)) {
-            status.textContent = "Please enter a valid email address.";
-            return false;
-        }
-
-        status.textContent = ""; // Clear status message if validation passes
-        return true;
-    };
-
-    // Function to handle form submission
     async function handleSubmit(event) {
         event.preventDefault();
-
-        // Validate the form before submission
-        if (!validateForm()) {
-            return;
-        }
-
         var data = new FormData(event.target);
         fetch(event.target.action, {
             method: form.method,
@@ -52,12 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
                     if (Object.hasOwn(data, 'errors')) {
                         status.textContent = data["errors"].map(error => error["message"]).join(", ");
                     } else {
-                        status.textContent = "Oops! There was a problem submitting your form.";
+                        status.textContent = "Oops! There was a problem submitting your form";
                     }
                 });
             }
         }).catch(error => {
-            status.textContent = "Oops! There was a problem submitting your form.";
+            status.textContent = "Oops! There was a problem submitting your form";
         });
     }
 
