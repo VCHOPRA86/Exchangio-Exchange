261fcf1de3e94a0ffd5b71fc0e785cb60bddce97
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2024-09-19 19:28:46 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -444,4 +444,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=false)  # Run the app in debug mode
+    app.run(debug=False)  # Run the app in debug mode
